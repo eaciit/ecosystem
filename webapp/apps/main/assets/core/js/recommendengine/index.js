7b221bfdfb96e1f97a5c65127e358f2c3aa0dfa8
joko prayitno <joko.prayitno@eaciit.com>
2017-10-27 14:42:24 +0700

    set recommend engine page 21 and 25 like on ppt

---
+++ webapp/apps/main/assets/core/js/recommendengine/index.js
@@ -0,0 +1,9 @@
+var recommendengine = {}
+
+recommendengine.onclickopportunity = function() {
+	$('#modalopportunity').modal('show')
+}
+
+recommendengine.onclickaddprogram = function() {
+	$('#modalopportunity').modal('show')
+}
