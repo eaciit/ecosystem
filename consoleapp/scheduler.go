af12e4a9ab0b35e0093ebf0ad372aa8783ba755b
Bagus Cahyono <bagus.cahyono@eaciit.com>
2018-01-22 15:52:41 +0800

    failsafe scheduler

--- consoleapp/scheduler.go
+++ consoleapp/scheduler.go
@@ -64,7 +64,7 @@ func (sc *Scheduler) generate() error {
 	sql = "RENAME TABLE " + readyTableName + " TO " + temp2TableName
 	qr = sqlh.Exec(sc.Db, sqlh.ExecNonScalar, sql)
 	if qr.Error() != nil {
-		return qr.Error()
+		fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: Failed to rename ready table to temp_2 table.", err.Error())
 	}
 
 	// Rename temp_1 to ready table
@@ -79,12 +79,12 @@ func (sc *Scheduler) generate() error {
 	fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: DONE!")
 
 	// Rename temp_2 to temp_1
-	fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: Rename temp_2 table to ready temp_1")
+	fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: Rename temp_2 table to ready temp_1 table")
 
 	sql = "RENAME TABLE " + temp2TableName + " TO " + temp1TableName
 	qr = sqlh.Exec(sc.Db, sqlh.ExecNonScalar, sql)
 	if qr.Error() != nil {
-		return qr.Error()
+		fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: Failed to rename rtemp_2 table to ready temp_1 table.", err.Error())
 	}
 
 	fmt.Println("-----> RECOMENDED ENGINE SCHEDULER: DONE!")
